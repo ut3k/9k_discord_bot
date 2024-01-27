00df85a7cef57a7e477c0f5bb7523c07af1a78ae
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2024-01-27 20:04:48 +0100

    z jakiegoś powodu nie działa autoryzacja

--- main.go
+++ main.go
@@ -1,20 +1,68 @@
 package main
 
 import (
-	"log"
-	"os"
-  "9k_discord_bot/bot"
-  
+  "fmt"
+  "log"
+  "os"
+  "os/signal"
+  "syscall"
 
-	"github.com/joho/godotenv"
+  "github.com/bwmarrin/discordgo"
+  "github.com/joho/godotenv"
 )
 
-func main () {
+
+
+func main() {
   err := godotenv.Load()
-  if err !=nil {
+  if err != nil {
     log.Fatal("Error loading .env file")
   }
-  bot.BotToken = os.Getenv("BOT_TOKEN")
-  bot.Run()
+
+  var BotToken string
+
+  BotToken = os.Getenv("BOT_TOKEN")
+  fmt.Println(BotToken)
+
+
+  dg, err := discordgo.New("Bot" + BotToken)
+  if err != nil {
+    fmt.Println("error creating Discord session")
+    return
+  }
+
+  // rejestrowanie jakiejś funkcji, do której będzie odwołanie
+  dg.AddHandler(messageCreate)
+
+  // to nie wiem co robi.
+  dg.Identify.Intents = discordgo.IntentGuildMessages
+
+  // otwarcie sesji i wywołanie ewentualnego błędu
+  err = dg.Open()
+  if err != nil {
+    fmt.Println("nie udało się otowrzyć połączenia, bo:", err)
+    return
+  }
+
+  // wywołanie przerwania, wyłączenia z terminalu bota
+  fmt.Println("Bot is running now... press CTR+C to stop")
+  sc := make(chan os.Signal, 1)
+  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+  <-sc
+
+  dg.Close()
 
 }
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
+
+  // omijanie własnych rekacji bota na siebie samego
+  if m.Content == "ping" {
+    s.ChannelMessageSend(m.ChannelID, "Pong !")
+  }
+  if m.Content == "pong" {
+    s.ChannelMessageSend(m.ChannelID, "ping !!")
+  }
+
+}
+
