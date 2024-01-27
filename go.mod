66698b07a7230d35a6cce2e1c139e116d2697077
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2024-01-27 02:37:23 +0100

    Dodanie .env

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/bwmarrin/discordgo v0.27.1 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"log"
+	"os"
+  "fmt"
+
+	// "github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
 
 func main () {
-  discord, err := discordgo.New("Bot " + "authentication token")
+  err := godotenv.Load()
+  if err !=nil {
+    log.Fatal("Error loading .env file")
+  }
+  BotToken := os.Getenv("BOT_TOKEN")
+
+  // discord, err := discordgo.New("Bot " + BotToken)
+  fmt.Println(BotToken)
 }
