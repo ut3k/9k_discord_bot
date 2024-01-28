e9f6054a42a45f5481661e4d7a4e58c2c80324cc
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2024-01-28 03:36:40 +0100

    przepisany kod, wywala autoryzacje

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func init(){
 }
 
 var  BotToken string = os.Getenv("BOT_TOKEN")
+var buffer = make ([][]byte, 0)
 
 func main () {
   
@@ -79,5 +80,148 @@ func ready (s *discordgo.Session, event *discordgo.Ready) {
   s.UpdateGameStatus(0, "!airhorn")
 
 }
-// pauza 
- // https://github.com/bwmarrin/discordgo/blob/master/examples/airhorn/main.go 
+
+func messageCreate (s *discordgo.Session, m *discordgo.MessageCreate) {
+
+  // ignore bot sefl msg
+  if m.Author.ID == s.State.User.ID {
+    return
+  }
+  
+ // check if msg is "!airHorn"
+  if strings.HasPrefix(m.Content, "!airhorn"){
+
+    // find chanel msg came from
+    c, err := s.State.Channel(m.ChannelID)
+    if err != nil {
+      return
+    }
+
+    // find guild channel
+    g, err := s.State.Guild(c.GuildID)
+    if err != nil {
+      return
+    }
+
+ // look for the message sender in that current guild current voice states
+    for _, vs := range g.VoiceStates {
+
+      if vs.UserID == m.Author.ID {
+        err = playSound(s, g.ID, vs.ChannelID)
+
+        if err != nil{
+          fmt.Println("error playing sound: ", err)
+        }
+        return
+
+
+
+
+      }
+    }
+
+  }
+
+}
+
+func guildCreate (s *discordgo.Session, event *discordgo.GuildCreate) {
+
+  if event.Guild.Unavailable {
+    return
+  }
+
+  for _, channel := range event.Guild.Channels {
+
+    if channel.ID == event.Guild.ID{
+      _, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! type !airhorn while in a voice channel to play sound")
+      return
+    }
+
+  }
+
+}
+
+
+func loadSound() error{
+
+  file, err := os.Open("airhorn.dca")
+  if err != nil {
+    fmt.Println("error opening DCA file: ", err)
+    return err
+
+  }
+
+  var opuslen int16
+
+  for {
+
+    // read opus frame lenght from dca file
+    err = binary.Read(file, binary.LittleEndian, &opuslen)
+
+
+    // if this is the end of file, just return.
+    if err == io.EOF || err == io.ErrUnexpectedEOF {
+      err := file.Close()
+
+      if err != nil {
+        return err
+      }
+
+      return nil
+
+    }
+
+    if err != nil {
+      fmt.Println("erro reading from DCA file: ", err)
+      return err
+
+    }
+
+    // read encoded pcm from DCA file
+    InBuf := make([]byte, opuslen)
+    err = binary.Read(file, binary.LittleEndian, &InBuf)
+    
+    // there should not be any of file errors
+    if err != nil {
+      fmt.Println("Error reading form DCA file: ", err)
+      return err
+    }
+
+    buffer = append(buffer, InBuf)
+  }
+
+
+}
+
+func playSound(s *discordgo.Session, GuildID, ChannelID string)(err error) {
+
+  // join the provided voice channel
+  vc, err := s.ChannelVoiceJoin(GuildID, ChannelID, false, true)
+  if err != nil {
+    return err
+  }
+
+  // sleep for specific amount of time before speaking
+  time.Sleep(250 * time.Millisecond)
+
+  // speak
+  vc.Speaking(true)
+
+  // send buffer data
+  for _, buff := range buffer{
+    vc.OpusSend <- buff
+  }
+
+  // stop speaking
+  vc.Speaking(false)
+
+  // sleep for specific amount of time before ending
+  time.Sleep(250 * time.Millisecond)
+
+  // disconnect from voice
+  vc.Disconnect()
+
+  return nil
+
+
+}
